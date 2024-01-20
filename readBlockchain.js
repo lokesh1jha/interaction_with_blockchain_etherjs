6266a544c647a258379f2dbabec55d69d69628b2
lokeshjha <lokesh1jha@gmai.com>
2024-01-21 00:22:47 +0530

    ether veresion depreciated to 5.7.2

--- readBlockchain.js
+++ readBlockchain.js
@@ -1,15 +1,34 @@
-const { ethers } = require("ethers");
+const { ethers, formatUnits } = require("ethers");
 
 //v5
-// const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/b8ee69377a2e4a1aa06d5f5ae0141fd3");
+const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/b8ee69377a2e4a1aa06d5f5ae0141fd3");
 
 //v6
-const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/b8ee69377a2e4a1aa06d5f5ae0141fd3");
+// const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/b8ee69377a2e4a1aa06d5f5ae0141fd3");
 
 const queryBlockchain = async () => {
-    const block = await provider.getBlockNumber();
-    console.log("Current Block Number: ", block);
+    try {
+        // Calling this function creates a block in real, could be checked on Etherscan.io website
+        // const block = await provider.getBlockNumber();
+        // console.log("Current Block Number: ", block);
+
+        // Know Balance
+        const balance = await provider.getBalance("0x71C7656EC7ab88b098defB751B7401B5f6d8976F")//wallet address
+        console.log("Account Balance in BN: ", balance.toString())
+        
+        const balanceEther = ethers.utils.formatEther(balance) //before v6 as it was not running on v6
+        // const balanceEther = formatUnits(balance, "ether") // Ether v6
+        console.log('Account Balance In Ether: ', balanceEther)
+
+        const amountInWei = ethers.utils.parseEther(balanceEther);
+        console.log('Account Balance In Wei: ', amountInWei.toString())
+
+
+
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 queryBlockchain();
-// Calling this function creates a block in real, could be checked on Etherscan.io website
